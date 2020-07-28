1a8d15ed2a8fab19f986fbb1937fcce363467a5a
David Fuentes <davidf0827@gmail.com>
2020-07-28 08:40:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ["one","two","three","four"] 
+  array.unshift element'
 end
 
 def remove_element_from_end_of_array(array)
